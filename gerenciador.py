451fa5873c8cee3312d4d8c500ef3201183c5d91
Vinícius Borges <vinib.almeida@gmail.com>
2025-04-14 20:36:02 -0300

    funções finalizadas com sucesso

--- gerenciador.py
+++ gerenciador.py
@@ -19,14 +19,30 @@ def ver_tarefas(tarefas):
         status = "✓" if tarefa["completada"] else " "
         nome_tarefa = tarefa["nome"] 
         print(f"{indice}.[{status}] {nome_tarefa}")
-        return
+
 
 def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
     indice_tarefa_ajustado = int(indice_tarefa) - 1
-    tarefas[indice_tarefa]["nome"] = novo_nome_tarefa
-    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    if indice_tarefa_ajustado >=0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado]["nome"] = novo_nome_tarefa
+        print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    else:
+        print("Índice de tarefa errado")
     return
 
+def completar_tarefa(tarefas, indice_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print(f"Tarefa {indice_tarefa} marcada como completada")
+    return
+
+def deletar_tarefas_completadas(tarefas):
+    print("Tarefas completadas foram deletadas")
+    for tarefa in tarefas:
+        if tarefa["completada"]: #igual a tarefa["completada"] == True
+            tarefas.remove(tarefa)
+    print("Tarefas completadas foram deletadas")
+    return
 
 
 #Armazenaremos todas as tarefas em uma lista
@@ -55,7 +71,15 @@ while True:
         indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
         novo_nome = input("Digite o novo nome da tarefa:")
         atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+    elif escolha == "4":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
+        completar_tarefa(tarefas, indice_tarefa)
+    elif escolha == "5":
+        deletar_tarefas_completadas(tarefas)
+        ver_tarefas(tarefas)
     elif escolha == "6":
+        print ("Programa finalizado!!")
         break
 
-    print ("Programa finalizado!!")
+  
