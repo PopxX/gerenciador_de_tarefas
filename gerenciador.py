59dcfa6a1f7a988aea81f7b46555cfaf0e7044fc
Vinícius Borges <vinib.almeida@gmail.com>
2025-04-14 16:55:17 -0300

    primeira versao do projeto

---
+++ gerenciador.py
@@ -0,0 +1,61 @@
+# FUNÇÕES
+
+def adicionar_tarefa(tarefas, nome_tarefa):
+
+    #nome_tarefa = "tarefa padrao") -> Utilizado para definir uma tarefa padrão caso o usuário não digite nada.
+
+    #tarefa: nome da tarefa
+    #completada: indicar se essa tarefa já foi completada ou não.
+
+    #Instanciando através de um dicionário.
+    tarefa = {"nome": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+    return
+
+def ver_tarefas(tarefas):
+    print("\nLista de Tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status = "✓" if tarefa["completada"] else " "
+        nome_tarefa = tarefa["nome"] 
+        print(f"{indice}.[{status}] {nome_tarefa}")
+        return
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    tarefas[indice_tarefa]["nome"] = novo_nome_tarefa
+    print(f"Tarefa {indice_tarefa} atualizada para {novo_nome_tarefa}")
+    return
+
+
+
+#Armazenaremos todas as tarefas em uma lista
+
+tarefas = []
+
+
+while True:
+    print("\n Menu do Gerenciador de Lista de tarefas:")
+    print("1 . Adicionar tarefa")
+    print("2 . Ver tarefas")
+    print("3 . Atualizar tarefa")
+    print("4 . Completar Tarefa")
+    print("5 . Deletar tarefas completadas")
+    print("6 . Sair")
+
+    escolha = input("Digite a sua escolha:")
+
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+    elif escolha == "3":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
+        novo_nome = input("Digite o novo nome da tarefa:")
+        atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+    elif escolha == "6":
+        break
+
+    print ("Programa finalizado!!")
